df994beb5d61637970efee2e3e6fea64b7d52846
Sarkis Derderian <sarkisderderian@hotmail.com>
2016-03-21 15:58:09 +0400

    Update connection.py

--- connection.py
+++ connection.py
@@ -6,7 +6,8 @@ def connect(company_name):
     #Connect 
     sessionManager = win32com.client.Dispatch("QBXMLRP2.RequestProcessor")
     sessionManager.OpenConnection('', company_name)
-    # i remove the first parameter (company_file_path) becuase i want to be only with opened file.
+    # i remove the first parameter (company_file_path) because i want to be only with opened file.
+    # i put second parameter 1 because i use multi user quickbooks Desktop
     ticket = sessionManager.BeginSession('', 1)
     return (sessionManager,ticket)
 
